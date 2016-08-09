6478acf0c703d29c2dcb6efe26e937cc3809e3fe
vikram singh <singh.vikram.0714@gmail.com>
2016-08-09 23:02:15 +0530

    about ganga finished

---
+++ app/src/main/java/action/namogange/AboutGanga.java
@@ -0,0 +1,29 @@
+package action.namogange;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by vicky on 8/8/2016.
+ */
+public class AboutGanga extends Fragment {
+
+    public AboutGanga() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.about_ganga, container, false);
+    }
+}
