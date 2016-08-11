fbb8431c5fbb13aeb70ab1ab63547aa141e559d1
vikram singh <singh.vikram.0714@gmail.com>
2016-08-11 22:19:11 +0530

    places to visit completed

--- app/src/main/java/action/namogange/DetailActivity.java
+++ app/src/main/java/action/namogange/DetailActivity.java
@@ -2,7 +2,6 @@ package action.namogange;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -26,8 +25,7 @@ public class DetailActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
